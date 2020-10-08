6c2cc29bc6ab4f986e43d88dc8e7e66ab4ee4490
randyle <le_randy@hotmail.com>
2020-10-08 15:37:09 -0600

    import font styles, some misc. cleanup

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -5,11 +5,12 @@ function Intro() {
     return (
         <div className={ 'intro' }>
             <div className={ `intro-text` }>
-                <h2>Randy Le</h2>
-                    <br/>
-                    welcome
-                    <br/>
-                    this is mostly just a playground for me to play around with libraries and designs
+                <h1>Randy Le</h1>
+                    <p>
+                        welcome
+                        <br/>
+                        this is mostly just a playground for me to play around with libraries and designs
+                    </p>
             </div>
             <div className={ `intro-image` }>
                 <img src={ require( './profile-pic.jpg' ) } alt={ `Profile` }/>
