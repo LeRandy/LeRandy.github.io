ac7265906a54bfe86ef950b50c9cdb0a3865a4ce
randyle <le_randy@hotmail.com>
2020-11-19 14:18:40 -0700

    Finished FlipCard component, takes in 2 props: front and back (both image urls). Added urls for FlipFade page

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -2,17 +2,43 @@ import React, { useState } from 'react'
 import { useSpring, animated as a } from 'react-spring'
 import './FlipCard.scss'
 
-function FlipCard () {
+/**
+ * props.back, props.front
+ */
+
+function FlipCard ( props ) {
   const [ flipped, set ] = useState( false );
+
   const { transform, opacity } = useSpring( {
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px) rotateX(${ flipped ? 180 : 0 }deg)`,
     config: { mass: 5, tension: 500, friction: 80 }
   } );
+
+  function url( address ) {
+    return `url(` + address + `)`;
+  }
+  
   return (
-    <div className={ `flip-card` } onClick={ () => set( state => !state ) }>
-      <a.div className="c back" style={ { opacity: opacity.interpolate(o => 1 - o), transform } }/>
-      <a.div className="c front" style={ { opacity, transform: transform.interpolate( t => `${t} rotateX(180deg)`) } }/>
+    <div className={ `flip-card` }>
+      <a.div 
+        className="c front" 
+        style={ { 
+          opacity, 
+          transform: transform.interpolate( t => `${t} rotateX(180deg)`),
+          backgroundImage: url( props.front )
+        } }
+        onClick={ () => set( state => !state ) }
+      />
+      <a.div 
+        className="c back" 
+        style={ { 
+          opacity: opacity.interpolate( o => 1 - o ), 
+          transform,
+          backgroundImage: url( props.back )
+        } }
+        onClick={ () => set( state => !state ) }
+      />
     </div>
   )
 }
--- src/components/main_content/content/flip_fade/FlipFade.tsx
+++ src/components/main_content/content/flip_fade/FlipFade.tsx
@@ -6,9 +6,13 @@ import Subheading from '../../../Subheading';
 function FlipFade() {
     return (
         <div>
-            <Subheading>*IN PROGRESS*</Subheading>
+            <Subheading>Flip Card</Subheading>
+            <p>I love dogs, I have 2 of them. This is Choji, he's a 9 year-old purebred Chihuahua.</p>
             <Showcase footnote={ `flip card: react-spring` }>
-                <FlipCard/>
+                <FlipCard
+                    front={ `//im.vsco.co/aws-us-west-2/148dbe/66644364/5fb6ddd301d61e328c000001/vsco5fb6ddd565f63.jpg?w=920&dpr=1` }
+                    back={ `https://image-aws-us-west-2.vsco.co/148dbe/66644364/5ee166be01d61e6c15000001/1136x1136/vsco5ee1667c88757.jpg` }
+                />
             </Showcase>
         </div>
     )
